c34a6550d949124b56f942a1ab7b6b1099b21948
yana <krotikyanka@gmail.com>
2025-05-14 17:45:00 +0300

    д

--- forms/translation.py
+++ forms/translation.py
@@ -5,8 +5,8 @@ from wtforms.validators import DataRequired
 
 class TransForm(FlaskForm):
     content = TextAreaField("Текст для перевода")
-    lang = SelectField(choices=[('English', 'English'), ('Russian', 'Russian'), ('Morsecode', 'Morsecode')])
-    trans_lang = SelectField(choices=[('English', 'English'), ('Russian', 'Russian'), ('Morsecode', 'Morsecode')])
+    lang = SelectField(choices=[('English', 'English'), ('Russian', 'Russian'), ('Morsecode', 'Morsecode')], default='Russian')
+    trans_lang = SelectField(choices=[('English', 'English'), ('Russian', 'Russian'), ('Morsecode', 'Morsecode')], default='Morsecode')
     submit = SubmitField('Перевести')
 
 
